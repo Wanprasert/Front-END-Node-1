9df29ba6b4aa587d6ecfcb29a01ffebaaa9fd53e
Juthapong <s6206021621091@email.kmutnb.ac.th>
2023-09-19 17:56:00 +0700

    update

--- AxiosNodeHtml.js
+++ AxiosNodeHtml.js
@@ -1,16 +1,20 @@
+// index.js
 // Description: Node.js HTML client
 // requires: npm install express ejs axios body-parser
 
 const express = require("express");
 const axios = require("axios");
-const app = express();
 var bodyParser = require("body-parser");
+const path = require("path");
+const app = express();
 
 // Base URL for the API
 //const base_url = "https://api.example.com";
 const base_url = "http://localhost:3000";
+//const base_url = "http://node41091-noderest.proen.app.ruk-com.cloud";
 
 // Set the template engine
+app.set("views", path.join(__dirname, "/public/views"));
 app.set("view engine", "ejs");
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
