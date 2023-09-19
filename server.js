9af7d8b61b7f38d162eb65eedb1965571e7bb5d6
Juthapong <s6206021621091@email.kmutnb.ac.th>
2023-09-19 18:11:53 +0700

    up

--- server.js
+++ server.js
@@ -5,7 +5,7 @@
 const express = require("express");
 const axios = require("axios");
 var bodyParser = require("body-parser");
-const path = require("path");
+
 const app = express();
 
 // Base URL for the API
@@ -14,7 +14,6 @@ const base_url = "https://node50297-juthapong.proen.app.ruk-com.cloud";
 //const base_url = "http://node41091-noderest.proen.app.ruk-com.cloud";
 
 // Set the template engine
-app.set("views", path.join(__dirname, "/public/views"));
 app.set("view engine", "ejs");
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
